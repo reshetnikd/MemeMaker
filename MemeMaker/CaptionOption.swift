3bc37ad35560b61544f5c4baddb0ca77110635d8
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-05 12:47:10 +0300

    Add data model

---
+++ MemeMaker/CaptionOption.swift
@@ -0,0 +1,13 @@
+//
+//  CaptionOption.swift
+//  MemeMaker
+//
+//  Created by Dmitry Reshetnik on 05.04.2021.
+//
+
+import Foundation
+
+struct CaptionOption {
+    var emoji: String
+    var caption: String
+}
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let topChoices = [CaptionOption(emoji: "🧑🏻‍💻", caption: "You never have to debug"), CaptionOption(emoji: "🤵🏻‍♂️", caption: "You can't get fired"), CaptionOption(emoji: "💵", caption: "You can't lose money")]
+    let bottomChoices = [CaptionOption(emoji: "💻", caption: "If you don't code"), CaptionOption(emoji: "🕺🏻", caption: "If you don't have a job"), CaptionOption(emoji: "🙅🏻‍♂️", caption: "If you never had any")]
+    
     @IBOutlet weak var topSegmentedControl: UISegmentedControl!
     @IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
     @IBOutlet weak var topCaptionLabel: UILabel!
