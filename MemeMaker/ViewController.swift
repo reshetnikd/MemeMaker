747329522c103a9998e631a82ad951137e8e110a
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-05 12:36:57 +0300

    Build the UI

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var topSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var topCaptionLabel: UILabel!
+    @IBOutlet weak var bottomCaptionLabel: UILabel!
+    
+    @IBAction func changeLabel(_ sender: UISegmentedControl) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
